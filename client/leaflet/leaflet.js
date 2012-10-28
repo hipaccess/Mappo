d87bba868a76ed2d6b24226304e763f4f22a3d4a
mstn <mstn@git.com>
2012-10-28 11:37:32 +0100

    fix map resize

--- client/leaflet/leaflet.js
+++ client/leaflet/leaflet.js
@@ -3,22 +3,18 @@ var MapView = Backbone.View.extend({
 	initialize: function() {
 		// generate a random id for this map
 		this.mapId_ = Math.random().toString(36).substring(7);
-		/*$(this.el).attr('id', this.mapId_);
-		$(this.el).attr('style', 'position: absolute; top: 0; left: 0; width: 100%; height: 100%;');*/
-		this.map = $('<div/>');
-		this.map.attr('id', this.mapId_);
-		this.map.attr('style', 'position: absolute; top: 0; left: 0; width: 100%; height: 100%;');
-		$(this.el).append(this.map);
-		// $(this.el).attr('style', 'position: absolute; top: 0; left: 0; width: 500px; height: 300px;');
+		$(this.el).attr('id', this.mapId_);
+		$(this.el).attr('style', 'position: absolute; top: 0; left: 0; width: 100%; height: 100%;');
 	},
 	render: function() {
-		
-		console.log( this.map[0] )
-		var map = L.map( this.map[0] , {
-			center: new L.LatLng(48, -3),
+		var map = L.map( this.el , {
+			center: new L.LatLng(0, 0),
 			zoom: 5,
 			attributionControl: true
 		});
+		
+		// https://github.com/CloudMade/Leaflet/issues/694
+		map._onResize()
 		
 		if ( L.plugins ){
 			for ( var name in L.plugins ){
