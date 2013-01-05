35a315f1855f63bdd5cdf32f6763e6f91864a2a7
mstn <mstn@git.com>
2013-01-05 22:40:09 +0100

    list of tile provider

--- client/Mappo.js
+++ client/Mappo.js
@@ -130,17 +130,37 @@ Meteor.startup(function () {
 	});
 	
 	Core.define('map-components', [ ], function(context){
-		
-		var osmMapAttr = '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>';
-		var osmDataAttr = 'Map data ' + osmMapAttr;
-		var layer = new L.TileLayer(
-			'http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', 
-			{attribution:osmMapAttr}
-		);
-		
+		Leaflet.load( 'providers', providerLeafletPlugin );
+		var defaultLayer = L.TileLayer.provider('OpenStreetMap.Mapnik');
+		var baseLayers = {
+				"OpenStreetMap Default": defaultLayer,
+				"OpenStreetMap German Style": L.TileLayer.provider('OpenStreetMap.DE'),
+				"OpenStreetMap Black and White": L.TileLayer.provider('OpenStreetMap.BlackAndWhite'),
+				"Thunderforest OpenCycleMap": L.TileLayer.provider('Thunderforest.OpenCycleMap'),
+				"Thunderforest Transport": L.TileLayer.provider('Thunderforest.Transport'),
+				"Thunderforest Landscape": L.TileLayer.provider('Thunderforest.Landscape'),
+				"MapQuest OSM": L.TileLayer.provider('MapQuestOpen.OSM'),
+				"MapQuest Aerial": L.TileLayer.provider('MapQuestOpen.Aerial'),
+				"MapBox Simple": L.TileLayer.provider('MapBox.Simple'),
+				"MapBox Streets": L.TileLayer.provider('MapBox.Streets'),
+				"MapBox Light": L.TileLayer.provider('MapBox.Light'),
+				"MapBox Lacquer": L.TileLayer.provider('MapBox.Lacquer'),
+				"MapBox Warden": L.TileLayer.provider('MapBox.Warden'),
+				"Stamen Toner": L.TileLayer.provider('Stamen.Toner'),
+				"Stamen Terrain": L.TileLayer.provider('Stamen.Terrain'),
+				"Stamen Watercolor": L.TileLayer.provider('Stamen.Watercolor'),
+				"Esri WorldStreetMap": L.TileLayer.provider('Esri.WorldStreetMap'),
+				"Esri DeLorme": L.TileLayer.provider('Esri.DeLorme'),
+				"Esri WorldTopoMap": L.TileLayer.provider('Esri.WorldTopoMap'),
+				"Esri WorldImagery": L.TileLayer.provider('Esri.WorldImagery'),
+				"Esri OceanBasemap": L.TileLayer.provider('Esri.OceanBasemap'),
+				"Esri NatGeoWorldMap": L.TileLayer.provider('Esri.NatGeoWorldMap')
+			};
+
 		return {
 			ready: function( map ){
-				map.addLayer( layer );
+				map.addLayer(defaultLayer);
+				map.addControl(new L.Control.Layers(baseLayers, '', {collapsed: true}));
 			}
 		};
 	});
