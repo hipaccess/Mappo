779b58715f7c415fea4252c212b2041e0b99d3a5
mstn <mstn@git.com>
2013-01-06 23:15:09 +0100

    faster implementation of osm module

--- client/Mappo.js
+++ client/Mappo.js
@@ -1,57 +1,55 @@
-
-
 // on document ready	
-Meteor.startup(function () {
-	
-	Core.define('auth', ['loginOk', 'loginKo'], function( context ){
+Meteor.startup(function() {
+
+	Core.define('auth', ['loginOk', 'loginKo'], function(context) {
 		return {
-			login: function(username, password){
-				if ( username === password ){
+			login: function(username, password) {
+				if (username === password) {
 					context.trigger('loginOk', username, 'faketoken');
 				} else {
 					context.trigger('loginKo');
 				}
 			},
-			handlerfake1: function(){
+			handlerfake1: function() {
 				// do something
-			}			
+			}
 		};
 	});
-	
-	Core.define('page', ['login', 'map'], function(context){
-		
+
+	Core.define('page', ['login', 'map'], function(context) {
+
 		// page root element
 		var page = $('body');
-		
+
 		return {
-			init: function(){
+			init: function() {
 				context.trigger('map', page);
 			},
-			click: function(){
+			click: function() {
 				context.trigger('login', 'marco', 'marco');
 			},
-			loginOk: function( username, token ){
+			loginOk: function(username, token) {
 				console.log('success: ' + token);
 			},
-			loginKo: function( ){
+			loginKo: function() {
 				console.log('login failed');
-			}			
+			}
 		};
 
 	});
-	
-	Core.define('map-viewer', [ 'ready' ], function(context){
-		
+
+	Core.define('map-viewer', ['ready'], function(context) {
+
 		// create a map
 		var mapId = Math.random().toString(36).substring(7);
 		var el = $('<div></div>');
 		el.attr('id', mapId);
 		el.attr('style', 'position: absolute; top: 0; left: 0; width: 100%; height: 100%;');
-	
-		
+
+
 		return {
-			render: function(){
-				var map = L.map( mapId, {
+			render: function() {
+				var map = L.map(mapId, {
 					center: new L.LatLng(46, 11),
 					zoom: 8,
 					attributionControl: true
@@ -60,39 +58,39 @@ Meteor.startup(function () {
 				map._onResize()
 				context.trigger('ready', map);
 			},
-			map: function( page ){
-				page.append( el );
+			map: function(page) {
+				page.append(el);
 				this.render();
 			}
 		};
 	});
-	
-	Core.define('mongomap', [], function(context){
-		var group = L.geoJson( null, null );
+
+	Core.define('mongomap', [], function(context) {
+		var group = L.geoJson(null, null);
 		var features = Collections.get('features');
 		return {
-			ready: function( map ){
+			ready: function(map) {
 				features.find().observe({
-							added: function(feature, beforeIndex) {
-								var layer = L.GeoJSON.geometryToLayer( feature );
-								group.addLayer( layer );
-							}
-						});
-				map.addLayer( group );
+					added: function(feature, beforeIndex) {
+						var layer = L.GeoJSON.geometryToLayer(feature);
+						group.addLayer(layer);
+					}
+				});
+				map.addLayer(group);
 				context.trigger('ready', map);
 			},
-			create: function( layer ){
+			create: function(layer) {
 				var feature = Util.formats.GeoJson.layerToGeometry(layer);
 				features.insert(feature);
 			}
-			
+
 		};
 	});
-	
-	Core.define('draw', [ 'create' ], function(context){
-		Leaflet.load( 'draw', drawLeafletPlugin );
+
+	Core.define('draw', ['create'], function(context) {
+		Leaflet.load('draw', drawLeafletPlugin);
 		return {
-			ready: function( map ){
+			ready: function(map) {
 				var drawControl = new L.Control.Draw({
 					position: 'topleft',
 					polygon: {
@@ -128,49 +126,124 @@ Meteor.startup(function () {
 			}
 		};
 	});
-	
-	Core.define('map-components', [ ], function(context){
-		Leaflet.load( 'providers', providerLeafletPlugin );
+
+	Core.define('map-components', [], function(context) {
+		Leaflet.load('providers', providerLeafletPlugin);
 		var defaultLayer = L.TileLayer.provider('OpenStreetMap.Mapnik');
 		var baseLayers = {
-				"OpenStreetMap Default": defaultLayer,
-				"OpenStreetMap German Style": L.TileLayer.provider('OpenStreetMap.DE'),
-				"OpenStreetMap Black and White": L.TileLayer.provider('OpenStreetMap.BlackAndWhite'),
-				"Thunderforest OpenCycleMap": L.TileLayer.provider('Thunderforest.OpenCycleMap'),
-				"Thunderforest Transport": L.TileLayer.provider('Thunderforest.Transport'),
-				"Thunderforest Landscape": L.TileLayer.provider('Thunderforest.Landscape'),
-				"MapQuest OSM": L.TileLayer.provider('MapQuestOpen.OSM'),
-				"MapQuest Aerial": L.TileLayer.provider('MapQuestOpen.Aerial'),
-				"MapBox Simple": L.TileLayer.provider('MapBox.Simple'),
-				"MapBox Streets": L.TileLayer.provider('MapBox.Streets'),
-				"MapBox Light": L.TileLayer.provider('MapBox.Light'),
-				"MapBox Lacquer": L.TileLayer.provider('MapBox.Lacquer'),
-				"MapBox Warden": L.TileLayer.provider('MapBox.Warden'),
-				"Stamen Toner": L.TileLayer.provider('Stamen.Toner'),
-				"Stamen Terrain": L.TileLayer.provider('Stamen.Terrain'),
-				"Stamen Watercolor": L.TileLayer.provider('Stamen.Watercolor'),
-				"Esri WorldStreetMap": L.TileLayer.provider('Esri.WorldStreetMap'),
-				"Esri DeLorme": L.TileLayer.provider('Esri.DeLorme'),
-				"Esri WorldTopoMap": L.TileLayer.provider('Esri.WorldTopoMap'),
-				"Esri WorldImagery": L.TileLayer.provider('Esri.WorldImagery'),
-				"Esri OceanBasemap": L.TileLayer.provider('Esri.OceanBasemap'),
-				"Esri NatGeoWorldMap": L.TileLayer.provider('Esri.NatGeoWorldMap')
-			};
+			"OpenStreetMap Default": defaultLayer,
+			"OpenStreetMap German Style": L.TileLayer.provider('OpenStreetMap.DE'),
+			"OpenStreetMap Black and White": L.TileLayer.provider('OpenStreetMap.BlackAndWhite'),
+			"Thunderforest OpenCycleMap": L.TileLayer.provider('Thunderforest.OpenCycleMap'),
+			"Thunderforest Transport": L.TileLayer.provider('Thunderforest.Transport'),
+			"Thunderforest Landscape": L.TileLayer.provider('Thunderforest.Landscape'),
+			"MapQuest OSM": L.TileLayer.provider('MapQuestOpen.OSM'),
+			"MapQuest Aerial": L.TileLayer.provider('MapQuestOpen.Aerial'),
+			"MapBox Simple": L.TileLayer.provider('MapBox.Simple'),
+			"MapBox Streets": L.TileLayer.provider('MapBox.Streets'),
+			"MapBox Light": L.TileLayer.provider('MapBox.Light'),
+			"MapBox Lacquer": L.TileLayer.provider('MapBox.Lacquer'),
+			"MapBox Warden": L.TileLayer.provider('MapBox.Warden'),
+			"Stamen Toner": L.TileLayer.provider('Stamen.Toner'),
+			"Stamen Terrain": L.TileLayer.provider('Stamen.Terrain'),
+			"Stamen Watercolor": L.TileLayer.provider('Stamen.Watercolor'),
+			"Esri WorldStreetMap": L.TileLayer.provider('Esri.WorldStreetMap'),
+			"Esri DeLorme": L.TileLayer.provider('Esri.DeLorme'),
+			"Esri WorldTopoMap": L.TileLayer.provider('Esri.WorldTopoMap'),
+			"Esri WorldImagery": L.TileLayer.provider('Esri.WorldImagery'),
+			"Esri OceanBasemap": L.TileLayer.provider('Esri.OceanBasemap'),
+			"Esri NatGeoWorldMap": L.TileLayer.provider('Esri.NatGeoWorldMap')
+		};
 
 		return {
-			ready: function( map ){
+			ready: function(map) {
 				map.addLayer(defaultLayer);
-				map.addControl(new L.Control.Layers(baseLayers, '', {collapsed: true}));
+				map.addControl(new L.Control.Layers(baseLayers, '', {
+					collapsed: true
+				}));
 			}
 		};
 	});
-	
-	Core.define('osm', [ ], function(context){
+
+	Core.define('osm', [], function(context) {
+		// response in MongoHQ cannot be bigger than 100 documents
+		var MAX_LIMIT = 100;
 		var geo = L.geoJson();
 		return {
-			ready: function( map ){
-				geo.addTo( map );
+			ready: function(map) {
+				// add osm layer to map
+				geo.addTo(map);
+				// get information about this collection
 				$.ajax({
+					url: 'https://api.mongohq.com/databases/osm/collections/relation?_apikey=ad1r3nxynhxls3vfq5q9',
+					dataType: 'json',
+					success: function(data) {
+						var count = data.count;
+						var ways = {};
+						var requests = [];
+						_.each(_.range(Math.ceil(count / MAX_LIMIT)), function(index) {
+							requests.push(function(callback) {
+								console.log('request ' + index);
+								$.ajax({
+									url: 'https://api.mongohq.com/databases/osm/collections/relation/documents?_apikey=ad1r3nxynhxls3vfq5q9&limit=100&skip=' + (index * 100),
+									dataType: 'json',
+									success: function(data) {
+										_.each(data, function(relation) {
+											_.each(relation.members, function(member) {
+												ways[ member.ref ] = true;
+											});
+										});
+										callback(null, true);
+									},
+									failure: function(data) {
+										console.error(data);
+										callback(null, false);
+									}
+								});
+							});
+						});
+						async.parallel(
+						requests, function(err, results) {
+							if (err) {
+								return console.log(err);
+							}
+							var keys = _.keys( ways );
+							// KEYS = keys;
+							var wayRequests = [];
+							// console.log( keys.length );
+							// console.log( Math.ceil(keys.length / 50) );
+							_.each(_.range(Math.ceil(keys.length / 50)), function(index) {
+									wayRequests.push(function(callback) {
+										// console.log( index + ': ' + JSON.stringify( keys.slice( index*50, (index+1)*50 ) ) );
+										var filter =  '{ "osmId": { "$in":' + JSON.stringify( keys.slice( index*50, (index+1)*50 ) ) + ' } }' ;	
+										$.ajax({
+											url: 'https://api.mongohq.com/databases/osm/collections/way/documents?_apikey=ad1r3nxynhxls3vfq5q9&q=' + encodeURIComponent( filter ),
+											success: function(data) {
+												_.each(data, function(way) {
+													geo.addData(way.geometry);
+												});
+												// console.log( filter );
+												callback(null, true);
+											},
+											failure: function(data) {
+												console.error(data);
+												callback(null, false);
+											}
+										});
+									});
+							});
+						   async.parallel( wayRequests, function(err, results){
+								if (err){
+									return console.error(err);
+								}
+						   });	
+						});
+						}, failure: function(data) {
+							console.error(data);
+						}
+					});
+
+/*$.ajax({
 					// max mongohq limit: 100 documents
 					url: 'https://api.mongohq.com/databases/osm/collections/relation/documents?_apikey=ad1r3nxynhxls3vfq5q9&limit=100',
 					dataType: 'json',
@@ -197,18 +270,17 @@ Meteor.startup(function () {
 					failure: function( data ){
 						console.log( data );
 					}
-				});
-			}
-		};
-	});
-	
+				});*/
+				}
+			};
+		});
+
 	Core.compose('mongo-draw', 'mongomap', 'draw');
 	Core.split('map', 'map-viewer', ['map-components', 'mongo-draw', 'osm']);
 	Core.compose('app', 'page', 'map');
-	
+
 	app = Core.start('app');
 	app.init();
-	
 
-});
 
+	});
